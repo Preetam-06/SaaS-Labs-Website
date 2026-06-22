cfb8943c1b2eb2dd444074ba9a6493f0175dab81
Preetam Omnarayan Prajapati <pritamop21@gmail.com>
2026-06-22 14:54:41 +0530

    Flask fix v1.2

--- backend/routes.py
+++ backend/routes.py
@@ -1,40 +1,37 @@
-from flask import Blueprint, render_template
-from flask import send_from_directory
-
+from flask import Blueprint, render_template, send_from_directory, Response
 import json
 import os
 
-
 routes = Blueprint("routes", __name__)
-
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def load_data():
-
     path = os.path.join(BASE_DIR, "blogs.json")
 
     with open(path, "r", encoding="utf-8") as file:
         return json.load(file)
 
 
-@app.route("/sitemap")
+@routes.route("/sitemap")
 def sitemap_page():
     return render_template("sitemap.html")
 
-@app.route("/robots.txt")
+
+@routes.route("/robots.txt")
 def robots():
     return send_from_directory("static", "robots.txt")
 
-@app.route("/sitemap.xml")
+
+@routes.route("/sitemap.xml")
 def sitemap_xml():
     return send_from_directory("static", "sitemap.xml")
+
 
 @routes.route("/")
 def index():
     return render_template("index.html")
-
 
 
 @routes.route("/about")
@@ -42,23 +39,19 @@ def about():
     return render_template("about.html")
 
 
-
 @routes.route("/services")
 def services():
     return render_template("services.html")
 
 
-
 @routes.route("/blogs")
 def blogs():
-
     data = load_data()
 
     return render_template(
         "blogs.html",
         blogs=data["blogs"]
     )
-
 
 
 @routes.route("/faqs")
